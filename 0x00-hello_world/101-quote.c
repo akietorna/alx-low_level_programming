62c7ea700f9d14a463a5aee4130964b1010781a8
akietorna <akietorna@gmail.com>
2023-06-16 10:18:12 -0700

    Create 101-quotes.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 /**
  *main - entry point to the program
- *write: does the output in place of printf
- *@void: expect nothing from the code
- *@filedes: is the file descriptor
- *@buf: is the buffer in which the string is written into
- *@nbyte: gives the number of bytes needed
+ *@void: returns nothing after compilling
  *Description: Outputing without using the printf
  *Return 1 which output to standard error
- *Return 0 which mean success
  */
 int main(void)
 {
-	int write(int filedes, const char *buf, unsigned int nbyte);
-
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
