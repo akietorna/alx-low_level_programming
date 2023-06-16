48c47f865d8721639b2fcf1de75c80e89fce813d
akietorna <akietorna@gmail.com>
2023-06-16 09:56:40 -0700

    Create 101-quotes.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,10 +10,10 @@
  *Return 1 which output to standard error
  *Return 0 which mean success
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
-
 int main(void)
 {
+	int write(int filedes, const char *buf, unsigned int nbyte);
+
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
