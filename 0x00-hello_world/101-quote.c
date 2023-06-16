ec5f6dd1d3c1249976c1aa84ea140dde028dac19
akietorna <akietorna@gmail.com>
2023-06-16 08:27:55 -0700

    Create 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,13 @@
  *@buf: is the buffer in which the string is written into
  *@nbyte: gives the number of bytes needed
  *Description: Outputing without using the printf
+ *Description: replaces printf
  *Return 1 which output to standard error
- *Return 0 which mean sucess
+ *Return 0 which mean success
  */
 int write(int filedes, const char *buf, unsigned int nbyte);
 
-int main(void)
+int main()
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
