9d0bd8337d1b0aa423063b556515d866752a6426
akietorna <akietorna@gmail.com>
2023-06-15 22:38:43 -0700

    Create 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
-int main()
+
+/*
+ * main - Entry point 
+ * 
+ *Description: 'prints a string'
+ *@parameter: use the printf function to print 
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
