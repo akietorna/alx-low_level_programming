0cf309ff6640856864ccec06fd79202db616e212
akietorna <akietorna@gmail.com>
2023-06-16 10:30:20 -0700

    Create 101-quotes.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  *main - entry point to the program
  *@void: returns nothing after compilling
  *Description: Outputing without using the printf
- *Return 1 which output to standard error
+ *Return: 1 which output to standard error
  */
 
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
