30269d3f63d4c7abf7f17f48feb83e67ce8f5ac5
akietorna <akietorna@gmail.com>
2023-06-16 02:41:46 -0700

    Work on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,9 @@
 
 /**
  *main - this is the starting point 
- *
  *Description: This program prints a string
  *@void: return nothing after compiling
- *
- *return:returns 0 if the codes run sucessfully
+ *return (0):returns 0 if the codes run sucessfully
 */
 int main(void)
 {
