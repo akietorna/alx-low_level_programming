4a792224b390246ea2f8786c5a13e920949b9a14
akietorna <akietorna@gmail.com>
2023-06-15 22:11:57 -0700

    Correct 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/*this is the beginning of my code*/
 int main(void)
 {
+	/* i brought the \ because i wanted to escape "*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
