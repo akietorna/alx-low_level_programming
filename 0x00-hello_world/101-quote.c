f4d3af7aa974f1a3009920893f34ab7e00de97a9
akietorna <akietorna@gmail.com>
2023-06-16 07:56:26 -0700

    Create 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main()
 {
-	write(1, "and that piece of art is useful\" - Dura Korpa, 2015-10-19\n", 58);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
