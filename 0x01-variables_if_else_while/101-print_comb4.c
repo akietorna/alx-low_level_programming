02f720158f6c0e3998c5e40b86b370969aeb5047
akietorna <akietorna@gmail.com>
2023-06-17 06:50:06 -0700

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ *main - entry point into the program
+ *Description: listing double digit numbers
+ *@void: expect nothing at the end of the code
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int my_num = '0';
+
+	while (my_num <= '9')
+	{
+		int my_num2 = '1';
+
+		while (my_num2 <= '9')
+		{
+			int my_num3 = '2';
+
+			while (my_num3 <= '9')
+			{
+				if (my_num != my_num2 && my_num != my_num3 && my_num2 != my_num3 && my_num < my_num2 && my_num2 < my_num3 && my_num < '8' && my_num2 < '9')
+				{
+					putchar(my_num);
+					putchar(my_num2);
+					putchar(my_num3);
+
+					if (my_num != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				my_num3++;
+			}
+
+			my_num2++;
+		}
+		my_num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
