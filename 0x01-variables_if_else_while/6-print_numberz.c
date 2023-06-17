c27b1ae564738fa55e5af087e1e366b44ba2985b
akietorna <akietorna@gmail.com>
2023-06-16 19:49:03 -0700

    Create 6-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - entry point into the program
+ *Description: listing single numbers
+ *@void: expect nothing at the end of the code
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char my_num = '0';
+
+	while (my_num <='9')
+	{
+		putchar(my_num);
+		my_num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
