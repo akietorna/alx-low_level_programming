8ff5f8db3c2881502f9a84a18bc7f3d3f53910bb
akietorna <akietorna@gmail.com>
2023-06-17 04:21:25 -0700

    Create 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,9 +21,14 @@ int main(void)
 			{
 				putchar(my_num);
 				putchar(my_num2);
-				putchar(',');
-				putchar(' ');
+				
+				if (my_num != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+			
 			my_num2++;
 		}
 
