21a7ebefc0471b1d7f8a47fe212267fc1d769992
akietorna <akietorna@gmail.com>
2023-06-17 03:47:29 -0700

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ *main - entry point into the program
+ *Description: listing double digit numbers
+ *@void: expect nothing at the end of the code
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int my_num = '0';
+
+	while (my_num <= '9')
+	{
+		int my_num2 = '1';
+
+		while (my_num2 <= '9')
+		{
+			if (my_num != my_num2 && my_num < my_num2 && my_num < '9')
+			{
+				putchar(my_num);
+				putchar(my_num2);
+				putchar(',');
+				putchar(' ');
+			}
+			my_num2++;
+		}
+
+		my_num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
