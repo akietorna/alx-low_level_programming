1b3c3417dc066154e2850634f22e46b05f6af014
akietorna <akietorna@gmail.com>
2023-06-22 08:43:02 -0700

    Create 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,12 +4,11 @@
  *_islower - checks if character is lower case
  *islower - checks if character is lower case and return 1 or 0
  *@c : inputed character
- *@w : inputed characther
  *Return: 1 if true and 0 if false
  */
 int islower(int c);
 
-int _islower(int w)
+int _islower(int c)
 {
 	return (islower(w) ? 1 : 0);
 }
