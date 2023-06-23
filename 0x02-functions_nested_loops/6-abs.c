8ec342bb43ab7159cd8aae7a2e0204c22112cab4
akietorna <akietorna@gmail.com>
2023-06-22 17:22:27 -0700

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_abs - compute absolute value of numbers
+ *abs - computes the absolute value of numbers
+ *@j: takes in number to be computered
+ *Return: Always zero(success)
+ */
+
+int abs(int j);
+
+int _abs(int j)
+{
+	return (abs(j));
+}
