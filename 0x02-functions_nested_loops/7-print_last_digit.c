061b0e0f16b3cc2dee3b40208f7d5082de9e7c99
akietorna <akietorna@gmail.com>
2023-06-25 15:52:09 -0700

    Created 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 
 /**
- *abs - prints absolute value of numbers
- *print_last_digit - prints the last digit of a number
+ *print_last_digit: prints the last digit of a number
  *@j: takes in a number
  *Return: the last digit
  */
-
-int abs(int j);
 
 int print_last_digit(int j)
 {
 	int last_char = abs(j) % 10;
 
-	_putchar(last_char + '0');
+	_putchar(last_char);
+	_putchar('0');
 
 	return (last_char);
 }
