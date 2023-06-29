d410cf7bcf5dbea5735efdc529b80d5027307c3e
akietorna <akietorna@gmail.com>
2023-06-29 06:15:22 -0700

    Create 9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,6 +5,8 @@
  *_strcpy - copies a string from a variable to another
  *@dest: where the string is copied to
  *@src: where the string is copied from
+ *
+ *Return: Always 0
  */
 
 char *_strcpy(char *dest, char *src)
