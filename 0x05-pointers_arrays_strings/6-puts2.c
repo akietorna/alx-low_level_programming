deb363ff1b1d584f2942e2fe28ff256cd8cc1889
akietorna <akietorna@gmail.com>
2023-06-29 03:23:22 -0700

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *puts2 - prints even characthers from a string
+ *@s: takes in string to be used
+ *
+ *Return: Always 0
+ */
+
+void puts2(char *s)
+{
+	int str_len = strlen(s);
+	int a = 0;
+
+	while (a < str_len)
+	{
+		if (a % 2 == 0)
+		{
+			char j = *(s + a);
+			_putchar(j);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
