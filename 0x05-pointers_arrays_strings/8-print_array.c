4a3d0e6d673cbcbbea9f5878d10541270ce721de
akietorna <akietorna@gmail.com>
2023-06-29 05:18:25 -0700

    Create 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,7 +15,14 @@ void print_array(int *a, int n)
 
 	while (b < n)
 	{
-		printf("%d, ", *(a + b));
+		if (b == (n - 1))
+		{
+			printf("%d", *(a + b));
+		}
+		else
+		{
+			printf("%d, ", *(a + b));
+		}
 		b++;
 	}
 	_putchar('\n');
