38ab2acd3a1d2478e141a052c091efacd4748f12
akietorna <akietorna@gmail.com>
2023-07-20 06:09:04 -0700

    Create 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,8 +2,7 @@
 
 
 /**
- *natural - sums multiples of 3 or 5
- *@n: takes the input
+ *main - sums multiples of 3 or 5
  *Return: Always(0)
  */
 
@@ -12,10 +11,10 @@ int main(void)
 	int n = 1024;
 	int result = 0;
 	int a;
-	
+
 	for (a = 0; a < n; a++)
 	{
-		if (a % 3 == 0 || a% 5 == 0)
+		if (a % 3 == 0 || a % 5 == 0)
 		{
 			result += a;
 		}
