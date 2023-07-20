891cd63daf779ed07f71ec3be9460919f636f474
akietorna <akietorna@gmail.com>
2023-07-20 08:29:07 -0700

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - prints the sum of even-valued terms in the fibonacci sequence
+ *Return: Always(0)
+ */
+
+int main(void)
+{
+	unsigned long int result = 0;
+	int term = 0;
+	unsigned long int a = 0;
+	unsigned long int b = 1;
+	unsigned long int temp = 0;
+
+	while (temp < 4000000)
+	{
+		temp = a + b;
+		a = b;
+		b = temp;
+
+		if (term % 2 == 0)
+		{
+			result += temp;
+		}
+		term++;
+	}
+	printf("%lu\n",result);
+	return (0);
+}
