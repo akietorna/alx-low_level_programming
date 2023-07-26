fa159825bc8dddedaf763a32fdf272659a204015
akietorna <akietorna@gmail.com>
2023-07-26 11:59:59 -0700

    Create 0-main.c main.h README.md

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,14 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: 0
+* Return: Always 0(success)
 */
 
 int main(void)
 {
         int i;
-
         i = 98;
+
         positive_or_negative(i);
 
         return (0);
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
-#include <stdio.h>
+carrie@ubuntu:/debugging$ cat main.h
 #ifndef MAIN_H
 #define MAIN_H
+
+//#include <stdio.h>
 
 void positive_or_negative(int i);
 
