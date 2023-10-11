678dabbd715332f28c8deb9e6ccaaeaad683e02b
akietorna <akietorna@gmail.com>
2023-10-11 15:50:22 -0700

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - prints an alphabet
+ * @c: characther to be printed
+ * Return c
+ */
+
+int _putchar(char *c)
+{
+	return (write(1,&c,1));
+}
