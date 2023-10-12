afe8db45b08ecfab97835f0cd8591a7a6a02e26c
akietorna <akietorna@gmail.com>
2023-10-11 17:36:46 -0700

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbrs from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar("\n");
+
+	return;
+}
