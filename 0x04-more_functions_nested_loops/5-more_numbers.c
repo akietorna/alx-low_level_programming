4085a3125069140cf0fb62974dffd3c22460cd1e
akietorna <akietorna@gmail.com>
2023-11-15 03:46:49 -0800

    Create 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,15 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (a = 0;a < 10; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b< 15; b++)
+		for (b = 0; b < 15; b++)
 		{
 			if (b > 9)
 			{
 				_putchar((b / 10) + '0');
 			}
-			_putchar((b % 10)+ '0');
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
