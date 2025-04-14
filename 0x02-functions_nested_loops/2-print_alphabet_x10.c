5ac37191bfce3b9fe625cc0314ed59d95a6e3192
akietorna <akietorna@gmail.com>
2025-04-14 06:15:34 -0700

    Print every minutes of the day

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -24,4 +24,3 @@ void print_alphabet_x10(void)
 		num++;
 	}
 }
-
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,46 +2,37 @@
 
 /**
  *jack_bauer - prints minutes in a day
- *
- *Return: Always 0(success)
+ *@void : no input
  */
 
 void jack_bauer(void)
 {
 	int a = 0;
 
-	while (a < 3)
+	while (a <= 2)
 	{
 		int b = 0;
 
-		while (b <= 9)
+		while (b <= 3)
 		{
-			if (a == 2 && b > 3)
-			{
-				break;
-			}
-			else
-			{
-				int c = 0;
+			int c = 0;
 
-				while (c < 6)
+			while (c <= 5)
+			{
+				int d = 0;
+
+				while (d <= 9)
 				{
-					int d = 0;
-
-					while (d <= 9)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(':');
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-						d++;
-					}
-					c++;
+					_putchar('0' + a);
+					_putchar('0' + b);
+					_putchar(':');
+					_putchar('0' + c);
+					_putchar('0' + d);
+					d++;
 				}
-				b++;
+				c++;
 			}
+			b++;
 		}
 		a++;
 	}
