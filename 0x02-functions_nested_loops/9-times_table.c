37f7758720e3a31f7b977c804ea1bf475306e96f
akietorna <akietorna@gmail.com>
2025-04-14 08:20:00 -0700

    Print times table up to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,14 @@
 #include "main.h"
 
 /**
- *big_value - prints value
- *@value: takes in the product of the nth value of the two loop
- *@b: takes in nth value of the 2nd loop
- *Return: Always 0(success)
- */
-
-
-void big_value(int value, int b);
-
-void big_value(int value, int b)
-{
-	if (value > 9)
-	{
-		int char_1 = (value / 10) % 10;
-		int char_2 = value % 10;
-
-		_putchar(' ');
-		_putchar(char_1 + '0');
-		_putchar(char_2 + '0');
-	}
-	else
-	{
-		if (b != 0)
-		{
-			_putchar(' ');
-			_putchar(' ');
-		}
-		_putchar(value + '0');
-	}
-}
-
-/**
- *times_table - prints nine times table
+ *times_table - prints 9 times tables from 0
+ *@void: no input
  */
 
 void times_table(void)
 {
-	int a = 0;
+	int res = 0;
+	int a  = 0;
 
 	while (a <= 9)
 	{
@@ -46,22 +16,31 @@ void times_table(void)
 
 		while (b <= 9)
 		{
-			int result = a * b;
+			res = a * b;
 
-			if (b == 9)
+			if (b == 0)
 			{
-				big_value(result, b);
+				_putchar('0');
 			}
-			else
+			else if (res <= 9)
 			{
-				big_value(result, b);
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + res);
+			}
+			else if (res > 9)
+			{
+				_putchar(' ');
+				_putchar('0' + (res / 10));
+				_putchar('0' + (res % 10));
+			}
+
+			if (b != 9)
+			{
 				_putchar(',');
 			}
-
 			b++;
 		}
-
-		_putchar('\n');
 		a++;
 	}
 }
