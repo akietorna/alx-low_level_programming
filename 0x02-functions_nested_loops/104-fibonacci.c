1377e95c45d0c48f9e0c0ccd745513c68757d5cd
akietorna <akietorna@gmail.com>
2025-04-15 07:27:00 -0700

    Print the first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,27 +7,28 @@
 
 int main(void)
 {
-	unsigned long int a = 0;
-	unsigned long int b = 1;
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	unsigned long int sum = 0;
 	int n = 0;
 
-	while (n < 98)
+	while (n < 96)
 	{
-		unsigned long int temp;
-
-		temp = a + b;
-
-		if (n == 97)
+		if (n < 95)
 		{
-			printf("%ld\n", temp);
+			sum = a + b;
+			printf("%lu, ", sum);
+			a = b;
+			b = sum;
 		}
 		else
 		{
-			printf("%ld, ", temp);
-		}
+			int temp = 0;
 
-		a = b;
-		b = temp;
+			sum = (a + b) / 100;
+			temp = (a + b) % 100;
+			printf("%lu%d\n", sum, temp);
+		}
 		n++;
 	}
 	return (0);
