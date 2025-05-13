f6d87409446b826011d2bf562bb98cd471a0e753
akietorna <akietorna@gmail.com>
2025-05-13 04:51:15 -0700

    Return the number of bytes in the initial segment of a string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *_strspn - checks the number of bytes in the initial segment of a string
+ *@s: the segment string placeholder
+ *@char: the string to compare the segment with
+ *Return: the number of bytes obtained
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0;
+	unsigned int count = 0;
+
+	while (s[a] != '\0')
+	{
+		unsigned int b = 0;
+		int status = 0;
+
+		while ((accept[b] != '\0') && (status == 0))
+		{
+			if (s[a] == accept[b])
+			{
+				count++;
+				status = 1;
+			}
+			b++;
+		}
+		if (status == 0)
+		{
+			break;
+		}
+		a++;
+	}
+	return (count);
+}
