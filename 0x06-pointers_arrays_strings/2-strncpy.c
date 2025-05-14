95b26eccbbfa88b37bd17a79703e857227d3516b
akietorna <akietorna@gmail.com>
2025-05-14 09:40:12 -0700

    Copy a string up to the nth byte

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,6 +12,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int a = 0;
+
+	while ((a < n) && (src[a] != '\0'))
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
 	return (dest);
 }
