08b43ad7e673a5f4fbaffeed9825143b5be0565e
akietorna <akietorna@gmail.com>
2025-05-14 09:58:33 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,5 +11,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	return (strcmp(s1, s2));
+	unsigned int a = 0;
+
+	while (s1[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+	return (0);
 }
