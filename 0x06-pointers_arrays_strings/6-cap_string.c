43a487d783a07dbef259a844804e9b7fe6d5a837
akietorna <akietorna@gmail.com>
2025-05-15 08:08:08 -0700

    Capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,10 @@ char *cap_string(char *s)
 {
 	unsigned int i = 0;
 
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] = s[0] - 32;
+	}
 	while (s[i] != '\0')
 	{
 		if (s[i] == ',' ||
@@ -29,11 +33,6 @@ char *cap_string(char *s)
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			{
 				s[i + 1] = s[i + 1] - 32;
-			}
-			else if ((s[i + 1] >= 'A' && s[i + 1] <= 'Z') ||
-				 (s[i + 1] >= '0' && s[i + 1] <= '9'))
-			{
-				s[i + 1] = s[i + 1];
 			}
 		}
 		i++;
