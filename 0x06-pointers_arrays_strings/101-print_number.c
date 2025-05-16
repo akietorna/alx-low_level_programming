b01a77e6eb1e728c80e003fe2da06efa7854e93d
akietorna <akietorna@gmail.com>
2025-05-15 21:15:32 -0700

    Print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,9 +11,12 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		n = -n;
 		_putchar('-');
-		print_number(n);
+		if (n / 10 != 0)
+		{
+			print_number(-(n / 10));
+		}
+		_putchar(-(n % 10) + '0');
 	}
 	else
 	{
