0ca014c14ac7f63adbadfe68a254b91991f90dde
akietorna <akietorna@gmail.com>
2025-06-25 08:54:43 -0700

    Write afunction that create a new struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include "dog.h"
 
+int str_len(char *str);
+void copystr(char *src, char *dest);
+
 /**
  *new_dog - create a new dog
  *@name: name of the dog
@@ -11,14 +14,68 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *dupname;
+	char *dupowner;
 	dog_t *dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
-	dog->name = name;
+	dupname = malloc((str_len(name) + 1) * sizeof(char));
+	if (dupname == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dupowner = malloc((str_len(owner) + 1) * sizeof(char));
+	if (dupowner == NULL)
+	{
+		free(dupname);
+		free(dog);
+		return (NULL);
+	}
+	copystr(name, dupname);
+	copystr(owner, dupowner);
+	dog->name = dupname;
+	dog->owner = dupowner;
 	dog->age = age;
-	dog->owner = owner;
 	return (dog);
+}
+
+/**
+ *str_len - finds length of a string
+ *@str: the string
+ *Return: length of the string
+ */
+
+int str_len(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
+
+
+/**
+ *copystr - copy a string
+ *@src: the source string
+ *@dest: destination string
+ *Return: Nothing
+ */
+
+void copystr(char *src, char *dest)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	dest[a] = '\0';
 }
