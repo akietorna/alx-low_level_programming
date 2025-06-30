0053044a40d34fde4ff72986f11e3a07757bf1e7
Akietorna <akietorna@gmail.com>
2025-06-30 10:42:31 -0700

    Perform simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ *main - main entry into functions
+ *@argc: number of argument
+ *@argv: the arguments
+ *Return: 0-success
+ */
+
+int main(int argc, char *argv[])
+{
+	int res = 0;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*(argv[2]) != '+' && *(argv[2]) != '-' && *(argv[2]) != '*'
+	    && *(argv[2]) != '/' && *(argv[2]) != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (argv[1] != NULL && argv[3] != NULL)
+	{
+		res = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+		printf("%i\n", res);
+	}
+ 	return (0);
+}
