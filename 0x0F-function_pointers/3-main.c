9a5ccb99781f08079ccb7f0c59e5223706f548ab
Akietorna <akietorna@gmail.com>
2025-06-30 14:24:30 -0700

    Perform simple operation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,8 +18,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (*(argv[2]) != '+' && *(argv[2]) != '-' && *(argv[2]) != '*'
-	     && *(argv[2]) != '/' && *(argv[2]) != '%' && argv[2][1] != '\0')
+	if (argv[2][1] != '\0' && *(argv[2]) != '+' && *(argv[2]) != '-'
+	    && *(argv[2]) != '*' && *(argv[2]) != '/' && *(argv[2]) != '%')
 	{
 		printf("Error\n");
 		exit(99);
