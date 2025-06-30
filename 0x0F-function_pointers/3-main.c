88563ff9aef12c9caa030491a1c4b0b2eed312ad
Akietorna <akietorna@gmail.com>
2025-06-30 14:15:44 -0700

    Perform simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,12 +19,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	if (*(argv[2]) != '+' && *(argv[2]) != '-' && *(argv[2]) != '*'
-	    && *(argv[2]) != '/' && *(argv[2]) != '%')
+	     && *(argv[2]) != '/' && *(argv[2]) != '%' && argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (argv[1] != NULL && argv[3] != NULL)
+	if (argv[3] != NULL)
 	{
 		res = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 		printf("%i\n", res);
