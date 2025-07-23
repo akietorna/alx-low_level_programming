5468c15c3ee9a7bcf75e5308e3ff38b4115697a9
Akietorna <akietorna@gmail.com>
2025-07-23 10:54:00 -0700

    Add a node at the end of a linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,74 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *add_node_end - adds a node to the end of a linked list
+ *@head: head node
+ *@str: string to be copied
+ *Return: pointer to the new node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp = *head;
+
+	if (temp == NULL)
+	{
+		temp = malloc(sizeof(list_t));
+		if  (temp == NULL)
+		{
+			return (NULL);
+		}
+		temp->str = strdup(str);
+		if (temp->str == NULL)
+		{
+			free(temp);
+			return (NULL);
+		}
+		temp->next = NULL;
+		temp->len = _strlen(temp->str);
+		*head = temp;
+		return (*head);
+	}
+	while (temp != NULL)
+	{
+		if (temp->next == NULL)
+		{
+			temp->next = malloc(sizeof(list_t));
+			if (temp->next == NULL)
+			{
+				return (NULL);
+			}
+			temp->next->str = strdup(str);
+			if (temp->next->str == NULL)
+			{
+				free(temp->next);
+				return(NULL);
+			}
+			temp->next->next = NULL;
+			temp->next->len = _strlen(temp->next->str);
+			break;
+		}
+		temp = temp->next;
+	}
+	return (temp->next);
+}
+
+
+/**
+ *_strlen - gets the length of a string
+ *@str: the string
+ *Return: length of the string
+ */
+
+int _strlen(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
