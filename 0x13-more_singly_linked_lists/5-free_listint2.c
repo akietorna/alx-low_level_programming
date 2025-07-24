1304a93fd56d094e3a3222e54b5eba4b4c9329c5
Akietorna <akietorna@gmail.com>
2025-07-24 11:16:42 -0700

    Free all memory including head itself

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *free_listint - frees a linked list
+ *@head: the head of the list
+ *Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	head = NULL;
+}
