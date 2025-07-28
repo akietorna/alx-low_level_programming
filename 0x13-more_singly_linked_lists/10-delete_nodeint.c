7cdf778e6a27a174b2c7723757910b48fcd17747
Akietorna <akietorna@gmail.com>
2025-07-28 13:55:49 -0700

    Delete a node at an index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	for (a = 0; a < (index -1); a++)
+	for (a = 0; a < (index - 1); a++)
 	{
 		if (temp == NULL || temp->next == NULL)
 		{
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		temp = temp->next;
 	}
-	if (temp->next == NULL )
+	if (temp->next == NULL)
 	{
 		return (-1);
 	}
