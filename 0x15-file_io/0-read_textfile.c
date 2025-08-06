5e6b682ae1829047861518785f1fdd7e0333f824
Akietorna <akietorna@gmail.com>
2025-08-06 11:01:40 -0700

    Read and write a file to standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,31 +13,39 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fdes, bw, br;
+	ssize_t fdes, bw, br;
 	char *buff = malloc(letters * sizeof(char));
 
+	if (buff == NULL)
+	{
+		return (0);
+	}
 	if (filename == NULL)
 	{
+		free(buff);
 		return (0);
 	}
 	fdes = open(filename, O_RDONLY);
 	if (fdes == -1)
 	{
-		close(fdes);
+		free(buff);
 		return (0);
 	}
 	br = read(fdes, buff, letters);
 	if (br == -1)
 	{
+		free(buff);
 		close(fdes);
 		return (0);
 	}
 	bw = write(1, buff, br);
-	if (bw != br)
+	if (bw == -1 || bw != br)
 	{
+		free(buff);
 		close(fdes);
 		return (0);
 	}
+	free(buff);
 	close(fdes);
-	return (br);
+	return (bw);
 }
