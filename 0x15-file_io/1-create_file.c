fb2bb5007fb449a73eff897f13d318105e1d66be
Akietorna <akietorna@gmail.com>
2025-08-08 08:36:54 -0700

    Create a file and write to it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ *create_file - creates a file
+ *@filename: the name of the file
+ *@text_content: content on the created file
+ *Return: -1-failure 1-success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fdes, bw;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fdes = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0600);
+	if (fdes == -1)
+	{
+		return (-1);
+	}
+	bw = write(fdes, text_content, _strlen(text_content));
+	if (bw == -1)
+	{
+		close(fdes);
+		return (-1);
+	}
+	close(fdes);
+	return (1);
+}
+
+/**
+ *_strlen - get length of a string
+ *@str: the string
+ *Return: length of the string
+ */
+
+int _strlen(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
