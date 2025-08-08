a39f7ac9372e625471e98395b188685109b37ff2
Akietorna <akietorna@gmail.com>
2025-08-08 08:42:57 -0700

    Create a file and write to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
 	bw = write(fdes, text_content, _strlen(text_content));
 	if (bw == -1)
 	{
