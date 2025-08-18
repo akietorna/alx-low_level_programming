7ea2714aa6fdf1fd139f2076ce2df6e51768d7dd
Akietorna <akietorna@gmail.com>
2025-08-18 06:34:53 -0700

    Read an elf header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,12 @@ int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 void check_file(int fdes1, int fdes2);
-
+void print_magic(header.e_ident);
+void print_class(header.e_ident);
+void print_data(header.e_ident);
+void print_version(header.e_ident);
+void print_os(header.e_ident);
+void print_type(header.e_type);
+void print_entry(header.e_entry);
 
 #endif
