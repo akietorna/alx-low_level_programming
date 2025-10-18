34bd3f9aef54517b465c1f0dc346e907905dce1e
akietorna <akietorna@gmail.com>
2025-10-18 16:08:05 +0000

    Free all nodes in a linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *free_dlistint - frees all node in a linked list
+ *@head: head node
+ *Return: Nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
